94ef8ceb7453d1152748c840aae537e180b9973f
quxl <quxl@msgcopy.com>
2015-03-20 17:19:33 +0800

    update settings  add DATABASES in production.py

--- settings/production.py
+++ settings/production.py
@@ -1,1 +1,14 @@
 from base import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cloudmusic',
+        'USER': 'quxl',
+        'PASSWORD': 'quxl',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command':'SET storage_engine=INNODB',
+        },
+    }
+}
