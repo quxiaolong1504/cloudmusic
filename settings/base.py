b25d789cf60e210ec0d5f29aa66467c9a959669a
quxl <quxl@msgcopy.com>
2015-03-23 16:38:59 +0800

    update:add auth 2.0 settings in settings/base.py

--- settings/base.py
+++ settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oauth2_provider',
     'account',
     'cmmedia',
     'music',
@@ -85,3 +86,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+    )
+}
